918b521166b88e427c8cb19662f507de7fb6bb4a
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 10:52:04 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -8,21 +8,22 @@ $(document).ready(
 		_QALET_.loadLet = function() {
 			var v = $('QALET'), r = {};	
 			for (var i = 0; i < v.length; i++) {
+				_QALET_._p++;
 				var data = $(v[i]).html();
 				if (!data) data = $(v[i]).attr('data');
 				var o = parse(data);
 				if (!_QALET_.lets[o.module]) {
 					r[o.module] = true;
+					_QALET_.lets[o.module] = true;
 				}	
-				_QALET_._p++;
-				_QALET_.lets[o.module] = true;
 				o.id = o.module + '_plugin_' + _QALET_._p;
 				_QALET_.data[o.id] = o;
 				$(v[i]).replaceWith('<div class="class_' + o.module +' '+o.id+'"></div>');
 			}
-			console.log(Object.keys(r))
-			console.log(_QALET_.data)
+
 		}
 		_QALET_.loadLet();
+		console.log(Object.keys(r))
+		console.log(_QALET_.data)
 	}
 );
