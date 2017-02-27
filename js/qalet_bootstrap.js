329aa3327f19a6b4ebb5f2212d9bf01a1068753c
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 10:24:59 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -1,21 +1,26 @@
+if !(_QALET_) _QALET_={lets:{}};
 $(document).ready(	
 	function() {		
 		function parse(v) {
 			var t = v.replace(/(“|”)/ig, '"');
 			return JSON.parse(t);
-		}			
-		var v = $('QALET'), r={}, f=[];	
-		for (var i = 0; i < v.length; i++) {
-			var data = $(v[i]).html();
-			if (!data) data = $(v[i]).attr('data');
-			var o = parse(data);
-			if (o.module) {
-				r[o.module] = true;
-				o.id = o.module + '_plugin_' + i;
-				f[f.length] = o;
-				$(v[i]).replaceWith('<div class="class_' + o.module +' '+o.id+'"></div>');
-				console.log(o);
+		}
+		_QALET_.addLet() {
+			var v = $('QALET'), r = {}, f = [];	
+			for (var i = 0; i < v.length; i++) {
+				var data = $(v[i]).html();
+				if (!data) data = $(v[i]).attr('data');
+				var o = parse(data);
+				if (!_QALET_.lets[o.module]) {
+					r[o.module] = true;
+					_QALET_.lets[o.module] = true;
+					o.id = o.module + '_plugin_' + i;
+					f[f.length] = o;
+					$(v[i]).replaceWith('<div class="class_' + o.module +' '+o.id+'"></div>');
+					console.log(o);
+				}
 			}
+			console.log(r)
 		}
 	}
 );
