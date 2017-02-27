5ea968ad06b9ca95cc454a87972ed2d23ce59f87
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 10:33:55 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -1,26 +1,28 @@
-if (!_QALET_) _QALET_={lets:{}};
+if (!_QALET_) _QALET_={lets:{}, _p:0, data:{}};
 $(document).ready(	
 	function() {		
 		function parse(v) {
 			var t = v.replace(/(“|”)/ig, '"');
 			return JSON.parse(t);
 		}
-		_QALET_.addLet() {
-			var v = $('QALET'), r = {}, f = [];	
+		_QALET_.addLet = function() {
+			var v = $('QALET'), r = {};	
 			for (var i = 0; i < v.length; i++) {
 				var data = $(v[i]).html();
 				if (!data) data = $(v[i]).attr('data');
 				var o = parse(data);
 				if (!_QALET_.lets[o.module]) {
 					r[o.module] = true;
+					_QALET_._p++;
 					_QALET_.lets[o.module] = true;
-					o.id = o.module + '_plugin_' + i;
-					f[f.length] = o;
+					o.id = o.module + '_plugin_' + _QALET_._p;
+					data[_QALET_._p] = o;
 					$(v[i]).replaceWith('<div class="class_' + o.module +' '+o.id+'"></div>');
 					console.log(o);
 				}
 			}
 			console.log(r)
 		}
+		_QALET_.addLet();
 	}
 );
