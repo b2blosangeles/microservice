f761c651f58d731764ce802de14dd7d50f27ca49
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 08:26:50 -0800

    add css

---
+++ package/wordpress_plugin.jsx
@@ -0,0 +1,45 @@
+{
+    "tpls":[],
+    "filelist":[
+		"/js/json2/json2.min.js",
+		"/js/jquery-2.1.3/jquery-2.1.3.js",
+	    "/js/UIQALET/css.js",
+		"/js/bootstrap.3.3.7/bootstrap.3.3.7.js",
+		"/js/qalet_plugin.js"
+      ],
+	"mservice": {
+		"giant_react":[],
+		"giant_angular":[],	
+		"giant_jsmarty":[
+			"/mservices/smarty/main.js"
+		]	
+	}, 
+	"dependence": {
+		"giant_react":[
+			"/js/react/react.min.js",
+			"/js/react/react-dom.min.js",
+			"/js/react/ReactRouter/3.0.2/ReactRouter.min.js"			
+			],
+		"giant_angular":[
+			"/js/angular-1.4.5/angular.min.js",
+			"/js/angular-1.4.5/angular-cookies.min.js",
+			"/js/angular-1.4.5/angular-route.min.js"
+		],	
+		"giant_jsmarty":[
+			"/js/jsmarty/smart-2.9.min.js"
+		],
+		"docviewer":[
+			"/js/react/react.min.js",
+			"/js/react/react-dom.min.js",
+			"/js/react/ReactRouter/3.0.2/ReactRouter.min.js"		
+		]	
+	},
+	"ms_tpls": {
+		"giant_angular":[
+
+		],
+		"giant_jsmarty":[
+
+		]
+	}	
+}
