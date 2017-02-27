68b0f3cee4ee179d4d87e2238a07f43168203c65
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 13:17:39 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -1,14 +1,14 @@
-if (!_Q_) var _Q_={lets:{}, _p:0, data:{}, _file:{}};
+if (!_QALET_) var _QALET_={lets:{}, _p:0, data:{}, _file:{}};
 $(document).ready(	
 	function() {		
 		function parse(v) {
 			var t = v.replace(/(“|”)/ig, '"');
 			return JSON.parse(t);
 		}
-		_Q_.loadLet = function() {
+		_QALET_.loadLet = function() {
 			var v = $('QALET'), r = {};	
 			for (var i = 0; i < v.length; i++) {
-				_Q_._p++;
+				_QALET_._p++;
 				var data = $(v[i]).html();
 				if (!data) data = $(v[i]).attr('data');
 				try {
@@ -23,12 +23,12 @@ $(document).ready(
 					$(v[i]).replaceWith('<div style="color:red">Miss module on '+ data + '</div>');
 					continue;
 				}
-				if (!_Q_.lets[o.module]) {
+				if (!_QALET_.lets[o.module]) {
 					r[o.module] = true;
-					_Q_.lets[o.module] = true;
+					_QALET_.lets[o.module] = true;
 				}	
-				o.id = o.module + '_plugin_' + _Q_._p;
-				_Q_.data[o.id] = o;
+				o.id = o.module + '_plugin_' + _QALET_._p;
+				_QALET_.data[o.id] = o;
 				$(v[i]).replaceWith('<div class="class_' + o.module +' '+o.id+'">'+o.module+'</div>');
 				console.log(o);
 			}
@@ -42,8 +42,8 @@ $(document).ready(
 					    function( data, textStatus, jqxhr ) {
 				  		console.log( "Load was performed." );
 					
-						for (var v in _Q_.data) {
-							var o = _Q_.data[v];
+						for (var v in _QALET_.data) {
+							var o = _QALET_.data[v];
 							if (typeof _QALET_[o.module] == 'function') {
 								console.log(1);
 								_QALET_[o.module](o);				
@@ -56,10 +56,10 @@ $(document).ready(
 			}	
 		}
 		
-		_Q_.loadLet();
+		_QALET_.loadLet();
 		setInterval(
 			function() {
-				_Q_.loadLet();
+				_QALET_.loadLet();
 				$('#niu').show();
 			}, 200
 		);
@@ -69,6 +69,6 @@ $(document).ready(
 		//		$('#niu').html('<qalet>{"module":"test"}</qalet>').hide();	
 			}
 		,4000);
-		console.log(_Q_.data);
+		console.log(_QALET_.data);
 	}
 );
