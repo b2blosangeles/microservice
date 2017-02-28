db0d7aaccfccc287c42fa8f25ae87ff1afcb9b67
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 16:27:50 -0800

    Rename wordpress_plugin.css to wordpress_plugin.js

---
+++ package/wordpress_plugin.js
@@ -0,0 +1,23 @@
+{
+	"tpls":[],
+	"filelist":[
+		
+	],
+	"mservice": {	
+		"giant_jsmarty":[
+			"/js/bootstrap.3.3.7/bootstrap.3.3.7.js",
+			"/mservices/smarty_A001/controller/main.js"
+		]	
+	}, 
+	"dependence": {
+		"giant_jsmarty":[
+			"/js/UIQALET/css.js",
+			"/js/jsmarty/smart-2.9.min.js"
+		]	
+	},
+	"ms_tpls": {
+		"giant_jsmarty":[
+			"/mservices/smarty_A001/view/infobox.html"
+		]
+	}	
+}
