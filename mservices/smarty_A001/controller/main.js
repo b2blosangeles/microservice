fae107586c795a3439ebad1ef937671e67501d49
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 18:22:22 -0800

    Update main.js

--- mservices/smarty_A001/controller/main.js
+++ mservices/smarty_A001/controller/main.js
@@ -3,7 +3,7 @@ var data = {
 };
 var tpl = new jSmart(_TPL_['/mservices/smarty_A001/view/infobox.html']);
 var res = tpl.fetch( data );
-$('.'+mapping_data.id).html(res);
+$('.'+mapping_data.id).html(res).load();
 setTimeout(
 	function() {
 		$('.'+mapping_data.id).show();
