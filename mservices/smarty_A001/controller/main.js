d8b89da8d63b90f0b2fd4119ef6b91fb3a4a6db3
b2blosangeles <b2blosangeles@gmail.com>
2017-02-28 07:38:06 -0800

    test

--- mservices/smarty_A001/controller/main.js
+++ mservices/smarty_A001/controller/main.js
@@ -1,6 +1,8 @@
 var data = {
 	lang:'en'
 };
+jSmart.prototype.left_delimiter = '[';
+jSmart.prototype.right_delimiter = ']';
 var tpl = new jSmart(_TPL_['/mservices/smarty_A001/view/infobox.html']);
 var res = tpl.fetch( data );
 $('.'+mapping_data.id).html(res);
