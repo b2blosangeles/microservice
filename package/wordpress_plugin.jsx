4f8ef972e8a78af1686971f93492262722776bab
b2blosangeles <b2blosangeles@gmail.com>
2017-02-27 17:26:06 -0800

    Update wordpress_plugin.jsx

--- package/wordpress_plugin.jsx
+++ package/wordpress_plugin.jsx
@@ -1,16 +1,15 @@
 {
 	"tpls":[],
 	"filelist":[
-		"/js/bootstrap.3.3.7/bootstrap.3.3.7.js"
 	],
 	"mservice": {	
 		"giant_jsmarty":[
-			
 			"/mservices/smarty_A001/controller/main.js"
 		]	
 	}, 
 	"dependence": {
 		"giant_jsmarty":[
+			"/js/bootstrap.3.3.7/bootstrap.3.3.7.js",
 			"/js/UIQALET/css.js",
 			"/js/jsmarty/smart-2.9.min.js"
 		]	
