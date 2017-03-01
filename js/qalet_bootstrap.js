fc2f9270789a27db23494e5cc967c84636c15ecb
b2blosangeles <b2blosangeles@gmail.com>
2017-03-01 14:28:57 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -35,6 +35,7 @@ $(document).ready(
 			console.log(_QALET_._newlet);
 			if (Object.keys(_QALET_._newlet).length) {	
 				for (var v in _QALET_._newlet) {
+					console.log(_QALET_._newlet[v]);
 					delete _QALET_._newlet[v];
 					var o = _QALET_.data[v];
 					if ((o.css) && (o.css.link)) {
