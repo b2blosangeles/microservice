ec60367a7f76c920f05046ac66193a5c82e35724
b2blosangeles <b2blosangeles@gmail.com>
2017-03-02 14:41:38 -0800

    Update main.js

--- mservices/smarty_A001/controller/main.js
+++ mservices/smarty_A001/controller/main.js
@@ -6,5 +6,11 @@ jSmart.prototype.right_delimiter = ']';
 var tpl = new jSmart(_TPL_['/mservices/smarty_A001/view/infobox.html']);
 var res = tpl.fetch( data );
 $('.'+mapping_data.id).html(res);
-$('.dropdown-toggle').dropdown();
+setTimeout(
+	function() {
+		$('.dropdown-toggle').dropdown();
+		console.log('==try it =');
+	},1000
+);	
+
 $('.'+mapping_data.id).show(0);
