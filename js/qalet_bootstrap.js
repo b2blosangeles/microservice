10aea2bc97f60ca46e2cc5c5ef852b062d016f97
b2blosangeles <b2blosangeles@gmail.com>
2017-03-02 07:01:26 -0800

    test

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -6,6 +6,7 @@ $(document).ready(
 		if (!_p[0]) _p = $("script[src$='/qalet_bootstrap.js']");
 		_QALET_.cfg.app = _p[0].src.replace(/\/qalet\_bootstrap(|\.min)\.js/, '/wordpress_plugin.jsx');
 		_QALET_.cfg.css = _p[0].src.replace(/\/qalet\_bootstrap(|\.min)\.js/, '/wordpress_plugin.css');
+		console.log(_QALET_.cfg);
 		_QALET_.customStyle = function (o) {
 			return function(data){
 				try {
