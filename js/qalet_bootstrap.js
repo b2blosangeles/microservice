3e3275c5bc865031505db27a3a494430b85841ee
b2blosangeles <b2blosangeles@gmail.com>
2017-03-01 18:10:45 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -32,6 +32,7 @@ $(document).ready(
 			}
 		}; 
 		_QALET_.callback = function() {
+			console.log('_QALET_._newlet===>');
 			console.log(_QALET_._newlet);
 			if (Object.keys(_QALET_._newlet).length) {	
 				for (var v in _QALET_._newlet) {
