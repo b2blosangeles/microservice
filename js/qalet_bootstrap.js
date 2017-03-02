3b5898aef532433accf991a005c32ea4593c4941
b2blosangeles <b2blosangeles@gmail.com>
2017-03-02 11:01:26 -0800

    Update qalet_bootstrap.js

--- js/qalet_bootstrap.js
+++ js/qalet_bootstrap.js
@@ -6,7 +6,7 @@ $(document).ready(
 		if (!_p[0]) _p = $("script[src$='/qalet_bootstrap.js']");
 		_QALET_.cfg.app = _p[0].src.replace(/\/qalet\_bootstrap(|\.min)\.js/, '/wordpress_plugin.jsx');
 		_QALET_.cfg.css = _p[0].src.replace(/\/qalet\_bootstrap(|\.min)\.js/, '/wordpress_plugin.css');
-		console.log(_QALET_.cfg);
+
 		_QALET_.customStyle = function (o) {
 			return function(data){
 				try {
@@ -57,7 +57,6 @@ $(document).ready(
 			if (Object.keys(_QALET_._newlet).length) {
 				return false;
 			}
-			console.log('-----_QALET_.loadLet()2;----' + v.length);
 			for (var i = 0; i < v.length; i++) {
 				_QALET_._p++;
 				var data = $(v[i]).html();
