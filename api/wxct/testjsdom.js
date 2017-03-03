a6ec14896802a3f9f9bae67b64ca1e8799b0e5a7
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 13:31:51 -0800

    Update testjsdom.js

--- api/wxct/testjsdom.js
+++ api/wxct/testjsdom.js
@@ -1,4 +1,9 @@
-pkg.request({ uri:'http://www.wenxuecity.com' }, function (error, response, body) {  
+var url = require("url");
+var url_src = 'http://www.wenxuecity.com';
+var p = url.parse(url_src); 
+res.send(p);
+return true;
+pkg.request({ uri:url_src  }, function (error, response, body) {  
   var jsdom = require(env.space_path + '/api/pkg/jsdom/node_modules/jsdom');
 
   if (error && response.statusCode !== 200) {
