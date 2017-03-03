952b3067a77c246acf59b7cd9331053c32ea228c
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 11:04:16 -0800

    Create smarty_news.js

---
+++ package/smarty_news.js
@@ -0,0 +1,19 @@
+{
+	"tpls":[],
+	"filelist":[
+	],
+	"mservice": {	
+		"jsmarty_news":[
+			"/mservices/jsmarty_news/controller/main.js"
+		]	
+	}, 
+	"dependence": {
+		"jsmarty_news":[
+		]	
+	},
+	"ms_tpls": {
+		"jsmarty_news":[
+			"/mservices/jsmarty_news/view/news_list.html"
+		]
+	}	
+}
