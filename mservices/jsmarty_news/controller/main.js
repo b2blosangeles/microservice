98b8bcafb3c85858590c6ec07d1f18383304bc16
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 11:26:11 -0800

    Update main.js

--- mservices/jsmarty_news/controller/main.js
+++ mservices/jsmarty_news/controller/main.js
@@ -1,21 +1,11 @@
 var data = {
 	lang:'en'
 };
-$.ajax({url: 'http://m.qalet.com/api/wxct/testjsdom.js', dataType:'json', success: function(result,status,xhr){
-	console.log(result);
+$.ajax({url: 'http://m.qalet.com/api/wxct/testjsdom.js', dataType:'json', success: function(data,status,xhr){
+	jSmart.prototype.left_delimiter = '[';
+	jSmart.prototype.right_delimiter = ']';
+	var tpl = new jSmart(_TPL_['/mservices/jsmarty_news/view/news_list.html']);
+	var html = tpl.fetch( {data:data} );
+	$('.'+mapping_data.id).html(html);
+	$('.'+mapping_data.id).show(0);
 }});
-
-
-jSmart.prototype.left_delimiter = '[';
-jSmart.prototype.right_delimiter = ']';
-var tpl = new jSmart(_TPL_['/mservices/jsmarty_news/view/news_list.html']);
-var res = tpl.fetch( data );
-$('.'+mapping_data.id).html(res);
-setTimeout(
-	function() {
-		$('.'+mapping_data.id).find('.dropdown-toggle').dropdown();
-		console.log('==try it 2 =');
-	},1000
-);	
-
-$('.'+mapping_data.id).show(0);
