13583778ca69a949e95520bd35b9fbca2164fcad
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 14:00:29 -0800

    Update testjsdom.js

--- api/wxct/testjsdom.js
+++ api/wxct/testjsdom.js
@@ -1,7 +1,7 @@
 var url = require("url");
 var url_src = 'http://www.wenxuecity.com';
 var p0 = url.parse( url_src); 
-res.send( url.parse( 'url_src/asa'));
+res.send( url.parse( '/url_src/asa'));
 return true;
 pkg.request({ uri:url_src  }, function (error, response, body) {  
   var jsdom = require(env.space_path + '/api/pkg/jsdom/node_modules/jsdom');
@@ -24,7 +24,7 @@ pkg.request({ uri:url_src  }, function (error, response, body) {
 			for (var i = 0; i < objs.length; i++) {
 				var href = $(objs[i]).attr('href');
 				var p = url.parse(href); 
-				if (!p['protocol']) href = p0.protocol + '//' + p0.host + href.replace(/^\//, '/');
+				if (!p['protocol']) href = p0.protocol + '//' + p0.host + ((!p.slashes)?p0.path:'') +  href;
 				result[result.length] = {href:href, text:$(objs[i]).html()};
 			}
 			res.send(result);
