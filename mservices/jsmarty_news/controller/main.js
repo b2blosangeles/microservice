ce5b9ee9d0be95e717bb2877cccd9191d8e18f79
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 22:25:47 -0800

    test

--- mservices/jsmarty_news/controller/main.js
+++ mservices/jsmarty_news/controller/main.js
@@ -9,6 +9,9 @@ var showData = function(list, doc) {
 	var html = tpl.fetch( {list:list, doc:doc} );
 	$('.'+mapping_data.id).html(html);
 	$('.'+mapping_data.id).show(0);
+	$('.'+mapping_data.id).find("a").bind("click", function() {
+                showDoc($(this).attr('data'), list);
+        });		
 };
 
 var showDoc = function(v, list) {
@@ -22,9 +25,7 @@ var showDoc = function(v, list) {
 
 $.ajax({url: 'http://m.qalet.com/api/newsfeed/wxct/wxct_list.js', dataType:'json', success: function(data,status,xhr){
 	showData(data,{});
-	$('.'+mapping_data.id).find("a").bind("click", function() {
-                showDoc($(this).attr('data'), data);
-        });	
+
 		
 //	$($('.'+mapping_data.id).find("a")[0]).click();
 }});
