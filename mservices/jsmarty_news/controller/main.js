165b5107424209eb8c2643007b58aa7bc1464a2d
b2blosangeles <b2blosangeles@gmail.com>
2017-03-03 22:06:37 -0800

    test

--- mservices/jsmarty_news/controller/main.js
+++ mservices/jsmarty_news/controller/main.js
@@ -2,6 +2,15 @@ var data = {
 	lang:'en'
 };
 
+var showData = function(list) {
+	jSmart.prototype.left_delimiter = '[';
+	jSmart.prototype.right_delimiter = ']';
+	var tpl = new jSmart(_TPL_['/mservices/jsmarty_news/view/news_list.html']);
+	var html = tpl.fetch( {data:list} );
+	$('.'+mapping_data.id).html(html);
+	$('.'+mapping_data.id).show(0);
+};
+
 var openUrl = function(v) {
 	$.ajax({url: 'http://m.qalet.com/api/newsfeed/wxct/wxct_page.js', data:{url:v},
 		dataType:'json', success: function(data,status,xhr){
@@ -11,13 +20,7 @@ var openUrl = function(v) {
 };
 
 $.ajax({url: 'http://m.qalet.com/api/newsfeed/wxct/wxct_list.js', dataType:'json', success: function(data,status,xhr){
-	jSmart.prototype.left_delimiter = '[';
-	jSmart.prototype.right_delimiter = ']';
-	var tpl = new jSmart(_TPL_['/mservices/jsmarty_news/view/news_list.html']);
-	var html = tpl.fetch( {data:data} );
-	$('.'+mapping_data.id).html(html);
-	$('.'+mapping_data.id).show(0);
-	
+	showData(data);
 	$('.'+mapping_data.id).find("a").bind("click", function() {
                 openUrl($(this).attr('data'));
         });	
