b9435178e533ef15e89f143063f6a9d9ac68afc6
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 20:42:32 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -3,7 +3,7 @@
 ,   cacheDirectory = "/tmp/cache";
 
 cachedRequest.setCacheDirectory(cacheDirectory);
-cacheRequest.setValue('ttl', 1000);
+cachedRequest.setValue('ttl', 1000);
 cachedRequest({url: 'https://www.google.com'}, 
 	function(err, data) {
 		res.send(data.body);
