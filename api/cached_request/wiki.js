ebbf90b341e94094558fbd204c34cf9270a26673
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 22:47:03 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -13,7 +13,7 @@ cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/google
 	}
 ); 
 */
-cachedRequest({url: 'https://www.google.com', encoding: 'binary'}, 
+cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.send(data);
 	//	res.end(Buffer.from(body, 'utf8'));
