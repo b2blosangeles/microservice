de1bb35eda43fce9ef0e249dab972f642d49b700
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 21:28:40 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -4,9 +4,13 @@
 
 cachedRequest.setCacheDirectory(cacheDirectory);
 cachedRequest.setValue('ttl', 1000);
+/*
 cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.writeHead(200, {'Content-Type': 'image/jpeg'});
 		res.end(body.toString('base64'));
 	}
 ); 
+*/
+cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', 
+encoding: 'binary'}).pipe(res); 
