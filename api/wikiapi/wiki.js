ce354c4b2880f3560159fd00ef0a766cc64d33dc
b2blosangeles <b2blosangeles@gmail.com>
2017-03-11 00:11:48 -0800

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,5 +1,4 @@
 ﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
-wikipedia.page.data("上海", { lang:'zh', cache:'/tmp' }, function(response) {
+wikipedia.page.data("上海", { content: true, lang:'zh', cache:'/tmp' }, function(response) {
 	res.send(response.text['*']);
 });
-// content: true, 
