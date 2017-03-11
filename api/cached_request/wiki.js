d6b4b34ccd64bb6bb8610981c4eaf55039671797
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 20:43:38 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -4,7 +4,7 @@
 
 cachedRequest.setCacheDirectory(cacheDirectory);
 cachedRequest.setValue('ttl', 1000);
-cachedRequest({url: 'https://www.google.com'}, 
+cachedRequest({url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Schematic_of_lymph_node_showing_lymph_sinuses.svg/610px-Schematic_of_lymph_node_showing_lymph_sinuses.svg.png'}, 
 	function(err, data) {
 		res.send(data.body);
 	}
