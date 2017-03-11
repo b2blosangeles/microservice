f999cc10348e3c5e6e0e10a603d55d4b5ee97d9e
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 23:12:26 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -16,7 +16,7 @@ cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/google
 cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.end(body);
-		res.end(Buffer.from(body, 'utf8'));
+		//res.end(Buffer.from(body, 'utf8'));
 	}
 ); 
 
