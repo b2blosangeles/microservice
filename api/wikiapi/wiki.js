89f3b64501ca58143d19bf0cf448e95d4118f45e
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 17:41:04 -0800

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,4 +1,4 @@
-var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
-wikipedia.page.data("Clifford_Brown", { content: true }, function(response) {
+﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
+wikipedia.page.data("上海", { content: true, language }, function(response) {
 	res.send(response.text['*']);
 });
