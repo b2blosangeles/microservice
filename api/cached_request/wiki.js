6a091b2e61643ac634b85f4e66df9755b4864029
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 23:47:31 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -1,35 +1,23 @@
-﻿var request = pkg.request
-,   cachedRequest = pkg.cachedRequest(request)
-,   cacheDirectory = "/tmp/cache";
+﻿var cache = pkg.cachedRequest(pkg.request);
 var url = req.query.url, pipe = req.query.pipe;
 if (!url) {
 	res.send('Miss url');
 	return true;
 }
-cachedRequest.setCacheDirectory(cacheDirectory);
-cachedRequest.setValue('ttl', 1000);
+cache.setCacheDirectory('/tmp/cache');
+cache.setValue('ttl', 1000);
 
-/*
-cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
-	function(err, data, body) {
-		res.writeHead(200, {'Content-Type': 'image/jpeg'});
-		res.end(body.toString('base64'));
-	}
-); 
-
-cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
-	function(err, data, body) {
-		res.end(body);
-		//res.end(Buffer.from(body, 'utf8'));
-	}
-); 
-*/
 if (pipe) {
-	cachedRequest({url: url}).pipe(res);
+	cache({url: url}).pipe(res);
 } else {
-	cachedRequest({url: url}, 
+	cache({url: url}, 
 		function(err, data, body) {
-			res.send(body);
+			if (err) {
+				res.send(err.message);
+			} else {
+				res.send(body);
+			}
+			
 		}
 	); 	
 }
