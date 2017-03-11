50c3b430539f61636b6ead3db8855819683bfa91
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 23:14:55 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -12,14 +12,13 @@ cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/google
 		res.end(body.toString('base64'));
 	}
 ); 
-*/
+
 cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.end(body);
 		//res.end(Buffer.from(body, 'utf8'));
 	}
 ); 
-
-/*
-cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png'}).pipe(res);
 */
+
+cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png'}).pipe(res);
