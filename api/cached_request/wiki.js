3270408011e06eba9d482844279cb1d13c4521a2
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 22:35:54 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -5,9 +5,17 @@
 cachedRequest.setCacheDirectory(cacheDirectory);
 cachedRequest.setValue('ttl', 300000);
 
-cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png'}, 
+/*
+cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
-		res.end(Buffer.from(body, 'utf8'));
+		res.writeHead(200, {'Content-Type': 'image/jpeg'});
+		res.end(body.toString('base64'));
+	}
+); 
+*/
+cachedRequest({url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Schematic_of_lymph_node_showing_lymph_sinuses.svg/610px-Schematic_of_lymph_node_showing_lymph_sinuses.svg.png'}, 
+	function(err, data, body) {
+		res.send(Buffer.from(body, 'utf8'));
 	}
 ); 
 
