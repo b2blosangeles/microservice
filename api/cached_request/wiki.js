dab34d9043a414f7a7f203c30849e330239af802
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 22:42:22 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -13,7 +13,7 @@ cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/google
 	}
 ); 
 */
-cachedRequest({url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Schematic_of_lymph_node_showing_lymph_sinuses.svg/610px-Schematic_of_lymph_node_showing_lymph_sinuses.svg.png', encoding: 'binary'}, 
+cachedRequest({url: 'https://www.google.com', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.send(data);
 	//	res.end(Buffer.from(body, 'utf8'));
