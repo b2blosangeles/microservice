85bac759231cfae86f376a2ee617de9559f1ed17
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 21:54:27 -0800

    test

---
+++ api/cached_request/f.js
@@ -0,0 +1,7 @@
+﻿var request = pkg.request;
+
+var url = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png';
+pkg.request.get(url).pipe(res); 
+/*
+res.writeHead(200, {'Content-Type': 'image/jpeg'});
+cachedRequest({url: 'https://www.google.com'}).pipe(res); */
