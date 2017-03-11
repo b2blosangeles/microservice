e1abc9406f1c459e6b4d4daa6f18e8742a16cc52
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 21:59:08 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -4,13 +4,13 @@
 
 cachedRequest.setCacheDirectory(cacheDirectory);
 cachedRequest.setValue('ttl', 1000);
-
+/*
 cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png', encoding: 'binary'}, 
 	function(err, data, body) {
 		res.writeHead(200, {'Content-Type': 'image/jpeg'});
 		res.end(body.toString('base64'));
 	}
 ); 
-/*
-res.writeHead(200, {'Content-Type': 'image/jpeg'});
-cachedRequest({url: 'https://www.google.com'}).pipe(res); */
+
+res.writeHead(200, {'Content-Type': 'image/jpeg'}); */
+cachedRequest({url: 'https://www.google.com'}).pipe(res);
