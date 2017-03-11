2c4ca5e80d140ff69918e339e9e41e48abafea6d
b2blosangeles <b2blosangeles@gmail.com>
2017-03-10 23:27:29 -0800

    test

--- api/cached_request/wiki.js
+++ api/cached_request/wiki.js
@@ -1,7 +1,11 @@
 ﻿var request = pkg.request
 ,   cachedRequest = pkg.cachedRequest(request)
 ,   cacheDirectory = "/tmp/cache";
-
+var url = req.query.url, pipe = req.query.pipe;
+if (!url) {
+	res.send('Miss url');
+	return true;
+}
 cachedRequest.setCacheDirectory(cacheDirectory);
 cachedRequest.setValue('ttl', 1000);
 
@@ -20,5 +24,12 @@ cachedRequest({url: 'https://www.google.com/images/branding/googlelogo/2x/google
 	}
 ); 
 */
-
-cachedRequest({url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Schematic_of_lymph_node_showing_lymph_sinuses.svg/610px-Schematic_of_lymph_node_showing_lymph_sinuses.svg.png'}).pipe(res);
+if (pipe) {
+	cachedRequest({url: url}).pipe(res);
+} else {
+	cachedRequest({url: url}, 
+		function(err, data, body) {
+			res.send(body);
+		}
+	); 	
+}
