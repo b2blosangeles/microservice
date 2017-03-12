344d8f71f2c93d95eb799e38ff577dc018fa70bb
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 00:39:36 -0800

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,5 +1,5 @@
 ﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
-wikipedia.page.data("Clifford_Brown", { content: true}, function(response) {
+wikipedia.page.data("Philadelphia_Phillies", { content: true}, function(response) {
 	res.send(response);
 });
 /*
