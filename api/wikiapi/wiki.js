1d5c8163f9371f5359a0ab734dedb7fcbb06c8d0
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 00:23:39 -0800

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,4 +1,4 @@
 ﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
 wikipedia.page.data("Clifford_Brown", function(response) {
-	res.send(respons);
+	res.send(response);
 });
