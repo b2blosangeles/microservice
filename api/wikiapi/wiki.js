22388cf0020cb977da3e789877efd3e0fe89dcd6
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 09:09:47 -0700

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -5,18 +5,11 @@ cache.setCacheDirectory('/tmp/cache/uii');
 cache.setValue('ttl', 1000);
 
 
+	
+
 
 wikipedia.page.data("shanghai", { content: true, lang:'en'}, function(a, b, c, d) {
-	cache({url: c}, 
-		function(err, data, body) {
-			if (err) {
-				res.send(err.message);
-			} else {
-				res.send(body);
-			}
-			
-		}
-	); 		
+	cache({url: c}).pipe(res);
 });
 /*
 wikipedia.categories.tree(
