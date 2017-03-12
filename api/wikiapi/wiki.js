be1a492f0098c631f1836b32304505c480b6e008
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 08:38:50 -0700

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,8 +1,6 @@
 ﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
-var urlparse = require("url");
-wikipedia.page.data("Philadelphia_Phillies", { content: true}, function(data) {
-	var u = "http://en.wikipedia.org/w/api.php" + urlparse.format({ query: {page:'Philadelphia_Phillies', format:'json'} });
-	res.send(u);
+wikipedia.page.data("Philadelphia_Phillies", { content: true}, function(response) {
+	res.send(response);
 });
 /*
 wikipedia.categories.tree(
