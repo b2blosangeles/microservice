fc43b0b81eebf302d345a94441d4474d61dc539b
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 00:31:25 -0800

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,4 +1,11 @@
 ﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
-wikipedia.page.data("Clifford_Brown", { content: true}, function(response) {
-	res.send(response);
-});
+//wikipedia.page.data("Clifford_Brown", { content: true}, function(response) {
+//	res.send(response);
+//});
+wikipedia.categories.tree(
+	"Philadelphia_Phillies",
+	function(tree) {
+		res.send(tree);
+		//nested data on the category page for all Phillies players
+	}
+);
