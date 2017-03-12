daccb754166c3e643e85db41d93c84a0b15a08a3
b2blosangeles <b2blosangeles@gmail.com>
2017-03-12 13:22:59 -0700

    test

--- api/wikiapi/wiki.js
+++ api/wikiapi/wiki.js
@@ -1,7 +1,25 @@
-﻿var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
+﻿/*
+var wikipedia = require(env.space_path + '/api/pkg/wikipedia/node_modules/node-wikipedia');
 wikipedia.page.data("Philadelphia_Phillies", { content: true}, function(response) {
 	res.send(response);
 });
+*/
+var channel = 'niu';
+var cache = pkg.cachedRequest(pkg.request);
+cache.setCacheDirectory('/tmp/cache' + ((channel)?'/'+channel:''));
+cache.setValue('ttl', 1000);
+var url = 'https://en.wikipedia.org/w/api.php?action=parse&page=shanghai&prop=categories|externallinks|links|text&lang=en&redirects=true&format=json';
+
+cache({url: url, encoding: 'binary'}, 
+	function(err, data, body) {
+		if (err) {
+			res.send(err.message);
+		} else {
+			res.send(body);
+		}
+		
+	}
+); 
 /*
 wikipedia.categories.tree(
 	"Philadelphia_Phillies",
