7c3323ae2774476cb41d20a97dd43e5ff2273f53
b2blosangeles <b2blosangeles@gmail.com>
2017-03-20 15:25:35 -0700

    Update main.jsx

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -19,7 +19,7 @@ try {
 		render: function() {
 			return (
 				<div>Test Itemdoc 
-					<button type="button" class="btn btn-primary btn-lg" onClick={showList}>Back</button>		
+					<button type="button" class="btn btn-primary btn-lg" onClick={this.showList}>Back</button>		
 				</div>
 			)
 		}	
