3f70df793e786468967e95cea68fb7882e0f7aa4
b2blosangeles <b2blosangeles@gmail.com>
2017-03-20 14:14:49 -0700

    Update main.jsx

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -19,7 +19,7 @@ try {
 				<div className="container-fluid">
 					<div className="row">
 						{this.state.list.map(function(item, index) {
-							return {item} = 
+							return <div>{item} =</div> 
 						})}		
 					</div>	
 				</div>
