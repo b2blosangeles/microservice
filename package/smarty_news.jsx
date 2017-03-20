e4408fa448c2702d2f2fb554d63bf91082ab057d
b2blosangeles <b2blosangeles@gmail.com>
2017-03-20 12:56:31 -0700

    Create smarty_news.jsx

---
+++ package/smarty_news.jsx
@@ -0,0 +1,28 @@
+{
+	"tpls":[],
+	"filelist":[
+		"/js/react/react.min.js",
+		"/js/react/react-dom.min.js"		
+	],
+	"mservice": {	
+		"jsmarty_news":[
+			"/mservices/jsmarty_news/controller/main.js"
+		],
+		"react_news":[
+			"/mservices/react_news/controller/main.jsx"
+		]			
+			
+	}, 
+	"dependence": {
+		"jsmarty_news":[
+		]	
+	},
+	"ms_tpls": {
+		"jsmarty_news":[
+			"/mservices/jsmarty_news/view/news_list.html"
+		],
+		"react_news":[
+			"/mservices/react_news/view/news_list.html"
+		]			
+	}	
+}
