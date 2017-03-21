21c0d3d017c36b17a428f7514040ddad15b3d416
b2blosangeles <b2blosangeles@gmail.com>
2017-03-21 12:55:20 -0700

    Update main.jsx

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -56,7 +56,7 @@ try {
 			var me = this;
 			return function() {
 				ReactDOM.render(
-					<div className="container-fluid info_bg body-film">
+					<div className="container-fluid qalet_box">
 						<div className="row">
 							<p className="info_box">	
 							{me.state.list.map(function(item, index) {
