d7f0ff50aa24f595b063325442a987df914c2084
b2blosangeles <b2blosangeles@gmail.com>
2017-03-21 10:42:50 -0700

    Update components.jsx

--- mservices/react_news/view/components.jsx
+++ mservices/react_news/view/components.jsx
@@ -1,9 +1,9 @@
-	var ListItem =  React.createClass({
-		render: function() {
-			return (
-				<div>
-					<a href="JavaScript:void(0)" onClick={this.props.parent.showDoc(this.props.item)}>{this.props.item.text}</a>
-				</div>
-			)
-		}	
-	});
+var ListItem =  React.createClass({
+	render: function() {
+		return (
+			<div>
+				<a href="JavaScript:void(0)" onClick={this.props.parent.showDoc(this.props.item)}>{this.props.item.text}</a>
+			</div>
+		)
+	}	
+});
