30fe9073a5a2d69f6993754318c7fc763d9cad18
b2blosangeles <b2blosangeles@gmail.com>
2017-03-21 07:52:37 -0700

    test

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -4,7 +4,7 @@ try {
 		render: function() {
 			return (
 				<div><a href="JavaScript:void(0)" onClick={this.props.parent.showDoc(this.props.item)}>
-					{this.props.item.text}</a>
+					{this.props.item.text} ==</a>
 				</div>
 			)
 		}	
