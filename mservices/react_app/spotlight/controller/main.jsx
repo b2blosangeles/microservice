4aaf3b64e1830cfea86d6532746bf2b5a2efc8f9
b2blosangeles <b2blosangeles@gmail.com>
2017-03-22 12:19:42 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -31,7 +31,7 @@ try {
 		render: function() {
 			var me = this;		
 			return (
-				<span className={'vp_'+mapping_data.id}>..</span>
+				<div className={'vp_'+mapping_data.id}>..</div>
 			  );
 		},
 		
