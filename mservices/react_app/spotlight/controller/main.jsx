756ebb6d110e66b2e9c86fbb98596090c68c562a
b2blosangeles <b2blosangeles@gmail.com>
2017-03-22 21:15:12 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -1,31 +1,5 @@
 try {	
-	var viewpoint = $('.'+mapping_data.id);
-	var Modal = React.createClass({
-		render: function() {
-			var me = this;		
-			return (
-				<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				  <div className="modal-dialog" role="document">
-				    <div className="modal-content">
-				      <div className="modal-header">
-					<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-					<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-					  <span aria-hidden="true">&times;</span>
-					</button>
-				      </div>
-				      <div className="modal-body">
-					...test...
-				      </div>
-				      <div className="modal-footer">
-					<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-					<button type="button" className="btn btn-primary">Save changes</button>
-				      </div>
-				    </div>
-				  </div>
-				</div>	
-			  );
-		}		
-	});	
+	var viewpoint = $('.'+mapping_data.id);	
 	var Home = React.createClass({
 		getInitialState: function() {
 			var me = this;
