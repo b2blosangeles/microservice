51247957b08c56be889e5290825096f5df1d3b66
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 05:34:11 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -31,17 +31,12 @@
 			return (
 				<div className="modal fade ModalAlert" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 				  <div className="modal-dialog" role="document">
-<div className="alert alert-success" role="alert">
-  <strong>Well done!</strong> You successfully read this important alert message.
-</div>						  
-					<div className="modal-content">		
-						
-					      <div className="modal-footer">
-					      
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
-					      </div>
-					 </div>	
+					<div className="alert alert-success" role="alert">
+						<strong>Well done!</strong> You successfully read this important alert message.
+						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+							  <span aria-hidden="true">&times;</span>
+						</button>
+					</div>
 				  </div>
 				</div>	
 			  );
