46e5469bba41f9ee04f53c086efadedc29d646b7
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 05:23:55 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -31,16 +31,6 @@
 			return (
 				<div className="modal fade ModalAlert" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 				  <div className="modal-dialog" role="document">
-				    <div className="modal-content">
-				      <div className="modal-header">
-					<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-					<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-					  <span aria-hidden="true">&times;</span>
-					</button>
-				      </div>
-				      <div className="modal-body">
-					...ModalAlert...{JSON.stringify(me.props.data)}===
-				      </div>
 				      <div className="modal-footer">
 					<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
 					<button type="button" className="btn btn-primary">Save changes</button>
