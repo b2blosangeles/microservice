9925b6227ec301ceedbfbbf7f79a62be5dda757f
b2blosangeles <b2blosangeles@gmail.com>
2017-03-22 17:53:58 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -39,7 +39,7 @@ try {
 					alert(90);
 					console.log('--bbj-->');
 					console.log(bbj);
-					bbj.show();
+					$(bbj).show();
 					 bbj.css({border:'6px solid red'});
 					alert(91);
 					bbj.hide();
