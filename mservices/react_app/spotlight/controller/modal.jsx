a5b057a271ea63e8c6c49e28dd29cb074df17edd
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 07:34:56 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -33,7 +33,7 @@
 				  <div className="modal-dialog" role="document">
 					<div className="alert alert-warning" role="alert">
 						<strong>!</strong> You successfully read this important alert message. 
-						{this.props.parent.state.appid}==> {this.props.config}
+						{this.props.parent.state.appid}==> {this.props.config()}
 						
 						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 							  <span aria-hidden="true">&times;</span>
