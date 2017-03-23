b6d3e6cebaff81aefca24a001aa029d8f38c6c04
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 06:49:38 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -41,6 +41,7 @@ try {
 							<button type="button" className="btn btn-success"
 								onClick={me.loadData.bind(this)} >Load Data</button>
 						</div>
+						{me.state.appid}
 						<ModalAlert data={{"title":"test title"}} config={me.state.appid} />
 						<GeneralModal data={{"title":"test title"}} />
 					</div>
