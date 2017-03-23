b9f31fe30c7dc285229e7d60553bc2661da8ca6e
b2blosangeles <b2blosangeles@gmail.com>
2017-03-22 20:38:28 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -26,7 +26,35 @@ try {
 					obj.attr('disabled', false);
 				});
 			},'json');
-		},	
+		},
+		popUp: function() {
+			var me = this;
+			return function() {
+				ReactDOM.render(
+					<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						  <span aria-hidden="true">&times;</span>
+						</button>
+					      </div>
+					      <div class="modal-body">
+						...
+					      </div>
+					      <div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					,
+					viewpoint.find('.vp_'+mapping_data.id)[0]
+				);
+			}	
+		},		
 		showList: function() {
 			var me = this;
 			return function() {
