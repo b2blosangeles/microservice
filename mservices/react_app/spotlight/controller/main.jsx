3c3cfeefc1a7a4fe2ce6ae41c2fa1f50c9cdd9fa
b2blosangeles <b2blosangeles@gmail.com>
2017-03-22 21:03:10 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -27,39 +27,6 @@ try {
 					obj.attr('disabled', false);
 				});
 			},'json');
-		},
-		popUp: function() {
-			var me = this;
-			return function() {
-				ReactDOM.render(
-					<span>	
-					<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-					  Launch demo modal
-					</button>					
-					<div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					  <div className="modal-dialog" role="document">
-					    <div className="modal-content">
-					      <div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-						  <span aria-hidden="true">&times;</span>
-						</button>
-					      </div>
-					      <div className="modal-body">
-						...
-					      </div>
-					      <div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
-					      </div>
-					    </div>
-					  </div>
-					</div>
-					</span>	
-					,
-					viewpoint.find('.vp_'+mapping_data.id)[0]
-				);
-			}	
 		},		
 		showList: function() {
 			var me = this;
