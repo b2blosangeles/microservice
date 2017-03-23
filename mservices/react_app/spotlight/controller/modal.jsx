6476bdbca1572f2c07f18e2a4f8179dfd7535a99
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 12:37:07 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -43,16 +43,16 @@
 		},
 		render: function() {
 			var me = this;	
-			var style = success;
+			var style = 'success';
 			
 			
 			return (
 				<div className="modal fade ModalAlert" tabindex="-1" role="dialog" aria-hidden="true">
 				  <div className="modal-dialog" role="document">
-					<div className="alert alert-{style}" role="alert">
+					<div className="alert alert-warning" role="alert">
 						<strong>!</strong> You successfully read this important alert message. 
 						{this.props.parent.state.ModalAlert}==>
-						-->{me.state.ModalAlertd}
+						-->{me.state.ModalAlertd}=={success}
 						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 							  <span aria-hidden="true">&times;</span>
 						</button>
