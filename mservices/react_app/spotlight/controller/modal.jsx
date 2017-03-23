1f1ff7dc12d8218799f02d5b2bbb378beab4130a
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 07:18:51 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -32,7 +32,7 @@
 				<div className="modal fade ModalAlert" tabindex="-1" role="dialog" aria-hidden="true">
 				  <div className="modal-dialog" role="document">
 					<div className="alert alert-warning" role="alert">
-						<strong>!</strong> You successfully read this important alert message. {me.state}==> {me.props.config}
+						<strong>!</strong> You successfully read this important alert message. {me.props.parent.state.appid}==> {me.props.config}
 						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
 							  <span aria-hidden="true">&times;</span>
 						</button>
