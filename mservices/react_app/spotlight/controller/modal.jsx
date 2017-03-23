2491af3c255b9e1acaf1ad1cf4ee20bb4992af04
b2blosangeles <b2blosangeles@gmail.com>
2017-03-23 08:27:29 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -28,7 +28,7 @@
 	var ModalAlert = React.createClass({
 		getInitialState: function() {
 			var me = this;
-			setInverval(function(){
+			setInterval(function(){
 				console.log(me.props.parent.inh()());
 			}, 2000);
 			
