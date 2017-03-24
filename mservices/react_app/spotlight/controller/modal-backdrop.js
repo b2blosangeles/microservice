337332132c7ec06f708fd1f38e2ad96b55600ff8
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 11:01:06 -0700

    Update modal-backdrop.js

--- mservices/react_app/spotlight/controller/modal-backdrop.js
+++ mservices/react_app/spotlight/controller/modal-backdrop.js
@@ -1,31 +1,26 @@
 (function() {
-	function checkcss(f) {
+	function getCssRule(f) {
 		var hasstyle = false;
 		var fullstylesheets = document.styleSheets;
 		for (var sx = 0; sx < fullstylesheets.length; sx++) {
 		    var sheetclasses = fullstylesheets[sx].rules || document.styleSheets[sx].cssRules;
 		    for (var cx = 0; cx < sheetclasses.length; cx++) {
-			if (sheetclasses[cx].selectorText == f) {
-			    hasstyle = true; break;            
+			if (sheetclasses[cx].selectorText == f) {	
+			//	hasstyle = true; break; 
+				hasstyle = sheetclasses[cx].cssText; break; 
 			}
 		    }
 		}
 		return hasstyle;
 	};
-	if (!checkcss('.modal-backdrop')) {
-		var modal_backdrop = ".modal-backdrop {position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 1040; background-color: #000000; }"+
+	var cssRule = getCssRule('.modal-backdrop')
+	if (!cssRule) {
+		cssRule = ".modal-backdrop {position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 1040; background-color: #000000; }"+
 		".modal-backdrop.fade {opacity: 0;} .modal-backdrop,.modal-backdrop.fade.in {opacity: 0.5;filter: alpha(opacity=50);}";
-		var style = $('<style>'+modal_backdrop+'</style>');
+		var style = $('<style>'+cssRule+'</style>');
 		$('html > head').append(style);
 	}
-	function getStyle(className) {
-	    var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
-	    for (var x = 0; x < classes.length; x++) {
-		if (classes[x].selectorText == className) {
-		    return (classes[x].cssText) ?classes[x].cssText : classes[x].style.cssText;
-		}
-	    }
-	}
-	console.log("getStyle('.modal-backdrop')===>");
-	console.log(getStyle('.modal-backdrop'));
+
+	console.log("cssRule===>");
+	console.log(cssRule);
 })();
