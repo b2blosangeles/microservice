6b7558ad97e8fb12ce2214926efe6c23b5464f6a
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 13:06:01 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -24,11 +24,8 @@ class ModalPlus extends React.Component {
 		var err_msg = '';
 		console.log('sub 8 ===');
 		if (_modal_backdrop_) {
-			if (me.state.ModalPlus.backdrop) {
-				_modal_backdrop_.setNew(me.state.ModalPlus.backdrop);	
-			} else {
-				if (_modal_backdrop_.default) $('html > head').append($('<style>'+ _modal_backdrop_.default+'</style>'));
-			}
+			if (me.state.ModalPlus.backdrop)  _modal_backdrop_.setNew(me.state.ModalPlus.backdrop);	
+			else  _modal_backdrop_.reset();
 		}
 		switch(me.state.ModalPlus.type) {
 			case "alert":
