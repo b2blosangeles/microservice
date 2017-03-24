8430a1e9ab5b179e7212e2fc4b2f36e1ad86a532
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 13:49:35 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -48,7 +48,25 @@ class ModalPlus extends React.Component {
 			case "popup":
 				if (!err_msg) {
 					return (			
-						<span></span>	
+						<div className="modal fade GeneralModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						  <div className="modal-dialog" role="document">
+						    <div className="modal-content">
+						      <div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+							<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+							  <span aria-hidden="true">&times;</span>
+							</button>
+						      </div>
+						      <div className="modal-body">
+							...GeneralModal..===
+						      </div>
+						      <div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="button" className="btn btn-primary">Save changes</button>
+						      </div>
+						    </div>
+						  </div>
+						</div>		
 					);
 				}
 				break;				
