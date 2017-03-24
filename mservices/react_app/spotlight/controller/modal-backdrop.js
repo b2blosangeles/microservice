bc54385ed3a6bb88b2091563e3602235cfc70abc
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 12:46:08 -0700

    Update modal-backdrop.js

--- mservices/react_app/spotlight/controller/modal-backdrop.js
+++ mservices/react_app/spotlight/controller/modal-backdrop.js
@@ -1,5 +1,22 @@
 /* if root domain does not support bootstrop we need add this part in */
-var _defaule_modal_backdrop;
+var _modal_backdrop_ = {
+	default:'',
+	setNew:function(cfg) {
+		var modal_backdrop = '';
+		if (cfg.bg) {
+			modal_backdrop += '.modal-backdrop {background-color: '+cfg.bg+'; }';
+		}
+		if (cfg.fade) {
+			modal_backdrop += '.modal-backdrop.fade {opacity: '+ cfg.fade +';}';
+		}
+		if (cfg.opacity) {
+			var opacity = cfg.opacity;
+			modal_backdrop += '.modal-backdrop.fade.in {opacity: '+ opacity +';filter: alpha(opacity=' + (opacity * 100) + ');}';
+		}
+		$('html > head').append( $('<style>'+modal_backdrop+'</style>'));
+		return modal_backdrop;
+	}
+};
 (function() {
 	function getCssRule(f) {
 		var hasstyle = '';
@@ -16,14 +33,14 @@ var _defaule_modal_backdrop;
 		return hasstyle;
 	};
 
-	_defaule_modal_backdrop = getCssRule('.modal-backdrop')+
+	_modal_backdrop_.default = getCssRule('.modal-backdrop')+
 		getCssRule('.modal-backdrop,.modal-backdrop.fade.in') + 
 		getCssRule('.modal-backdrop.fade');
 	
-	if (_defaule_modal_backdrop == '') {
-		_defaule_modal_backdrop = ".modal-backdrop {position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 1040; background-color: #000000; }"+
+	if (_modal_backdrop_.default == '') {
+		_modal_backdrop_.default = ".modal-backdrop {position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 1040; background-color: #000000; }"+
 		".modal-backdrop.fade {opacity: 0;} .modal-backdrop,.modal-backdrop.fade.in {opacity: 0.5;filter: alpha(opacity=50);}";
-		var style = $('<style>'+_defaule_modal_backdrop+'</style>');
-		$('html > head').append(style);
+		$('html > head').append( $('<style>'+_modal_backdrop_.default+'</style>'));
 	}
 })();
+
