dfb49535169afa3c2989bf65ad4fcea904281385
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 06:59:22 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -139,7 +139,7 @@ class ModalPlus extends React.Component {
 			viewpoint.find('.ModalPlus').modal({backdrop:'static'});
 		}
 	}
-	render function() {
+	render () {
 		var me = this;	
 		var style, message, showcloseicon;
 		switch(me.state.Modalcode) {
