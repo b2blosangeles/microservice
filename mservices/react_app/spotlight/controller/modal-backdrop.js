e8bdb2a37edb1ab6073cdf76f520ae68ff103bf9
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 10:53:29 -0700

    Update modal-backdrop.js

--- mservices/react_app/spotlight/controller/modal-backdrop.js
+++ mservices/react_app/spotlight/controller/modal-backdrop.js
@@ -18,5 +18,14 @@
 		var style = $('<style>'+modal_backdrop+'</style>');
 		$('html > head').append(style);
 	}
-
+	function getStyle(className) {
+	    var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+	    for (var x = 0; x < classes.length; x++) {
+		if (classes[x].selectorText == className) {
+		    (classes[x].cssText) ? alert(classes[x].cssText) : alert(classes[x].style.cssText);
+		}
+	    }
+	}
+	console.log("getStyle('.modal-backdrop')===>");
+	console.log(getStyle('.modal-backdrop'));
 })();
