7e2cbe7c2452e19b397691320e444a400209d50f
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 06:55:38 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -7,7 +7,7 @@ try {
 		}		
 		changeId(id) {
 			var me = this;
-			me.setState({ModalPlus: {code:'success', tm: new Date().getTime()}});
+			me.setState({ModalPlus: {code:'info', tm: new Date().getTime()}});
 		//	viewpoint.find('.ModalAlert').modal({backdrop:'static'});
 		}
 		loadData(e) {
