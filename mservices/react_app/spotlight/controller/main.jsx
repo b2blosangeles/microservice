3a9a4343fa0c65a5c598ba6cc12b25ec7523b500
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 16:25:03 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -12,9 +12,10 @@ try {
 						 message:'<img src="https://i.stack.imgur.com/oQ0tF.gif" width="24">',  id: id}});
 			setTimeout(
 				function() {
-					me.setState({ModalPlus: {type:'cancel', callback:function() {
-						me.setState({ModalPlus: {type:'alert', style:'success', message:'nice job', id: new Date().getTime()}});	
-					}}});	
+					me.setState({ModalPlus: {type:'alert', style:'success', message:'nice job', id: new Date().getTime()}});
+					//me.setState({ModalPlus: {type:'cancel', callback:function() {
+					//	me.setState({ModalPlus: {type:'alert', style:'success', message:'nice job', id: new Date().getTime()}});	
+					//}}});	
 				}, 10000
 			
 			)
