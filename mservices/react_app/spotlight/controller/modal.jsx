2338f793998e72a7bb13612f5e8452d1d29fc73f
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 14:46:33 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -3,7 +3,7 @@ class ModalPlus extends React.Component {
 		var me = super(props);
 		setInterval(function(){
 			if (me.props.parent.state.ModalPlus) {
-				if (typeof me.props.parent.state.ModalPlus == null) {
+				if (me.props.parent.state.ModalPlus == null) {
 					console.log('cancel it !');
 				} else if  (me.props.parent.state.ModalPlus !== me.state.ModalPlus) {
 						me.setState({ModalPlus: me.props.parent.state.ModalPlus });
