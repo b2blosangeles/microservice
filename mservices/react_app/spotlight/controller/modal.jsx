057ae6c8fd111105df31fd0581132d8405ea59f5
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 08:18:32 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -88,37 +88,43 @@ class ModalPlus extends React.Component {
 	}
 	render () {
 		var me = this;	
+		var err_msg = '';
 		switch(me.state.ModalPlus.type) {
 			case "alert":
 				var style = (me.state.ModalPlus.style)?me.state.ModalPlus.style:'info';
 				var message = (me.state.ModalPlus.message)?('<strong>!</strong> ' + me.state.ModalPlus.message):'<strong>!</strong>';
 				var showcloseicon = '';
-				return (			
-					<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
-					  <div className="modal-dialog" role="document">
-						<div className={'alert alert-' + style} role="alert">
-							<span dangerouslySetInnerHTML={{__html: message}}></span> ===
-							<button type="button" className="close" data-dismiss="modal" style={{display:showcloseicon}}>
-								&times;
-							</button>
-						</div>
-					  </div>
-					</div>	
-				)	
+				if (!err_msg) {
+					return (			
+						<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
+						  <div className="modal-dialog" role="document">
+							<div className={'alert alert-' + style} role="alert">
+								<span dangerouslySetInnerHTML={{__html: message}}></span> ===
+								<button type="button" className="close" data-dismiss="modal" style={{display:showcloseicon}}>
+									&times;
+								</button>
+							</div>
+						  </div>
+						</div>	
+					)
+				}
 				break;	
 			default:
-				return (
-					<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
-					  <div className="modal-dialog" role="document">
-						<div className="alert alert-danger" role="alert">
-							<strong>!</strong> wrong or missong ModelPlus Type
-							<button type="button" className="close" data-dismiss="modal">
-								&times;
-							</button>
-						</div>
-					  </div>
-					</div>	
-				)	
+				err_msg = 'wrong or missong ModelPlus Type';
 		} 
+		if (!err_msg) {
+			return (
+				<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
+				  <div className="modal-dialog" role="document">
+					<div className="alert alert-danger" role="alert">
+						<strong>!</strong> {err_msg}
+						<button type="button" className="close" data-dismiss="modal">
+							&times;
+						</button>
+					</div>
+				  </div>
+				</div>	
+			)
+		}	
 	}				
 }
