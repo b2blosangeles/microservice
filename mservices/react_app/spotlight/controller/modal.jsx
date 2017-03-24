566059ed9ecf3a52d33ad52d41843ebca9f46f0a
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 15:11:46 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -9,19 +9,19 @@ class ModalPlus extends React.Component {
 					viewpoint.find('.ModalPlus').modal('hide');
 					me.props.parent.state.ModalPlus = null;
 				} else {
-					if ((me.props.parent.state.ModalPlus.hold) && !me.props.parent.state.ModalPlus.startTime) {
-						me.props.parent.state.ModalPlus.startTime = new Date().getTime();
+					if (me.props.parent.state.ModalPlus.hold) {
+						if  (!me.props.parent.state.ModalPlus.startTime) {
+							me.props.parent.state.ModalPlus.startTime = new Date().getTime();
+						}
+						if  (new Date().getTime() < (me.props.parent.state.ModalPlus.hold + me.props.parent.state.ModalPlus.startTime)) {
+							return true;
+						}
 					}
 					if (me.props.parent.state.ModalPlus !== me.state.ModalPlus) {
 						me.setState({ModalPlus: me.props.parent.state.ModalPlus });
 					}	
 					
 				}	
-					
-	
-				//	if (!me.props.parent.state.ModalPlus)
-				//		me.setState({ModalPlus: me.props.parent.state.ModalPlus });
-				//	}	
 			}				
 		}, 100);
 		this.state = {ModalPlus: ''}
