c92065ecb7913f659e630aa55c7b3a3b993c2135
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 16:46:50 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -1,3 +1,30 @@
+class ModalContentes extends React.Component {
+	constructor(props) {
+		var me = super(props);	
+	}	
+	render() {
+		return(
+		<span>
+			<div className="modal-header">
+				<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+			</div>
+			<div className="modal-body">
+				.===
+			</div>
+			<div className="modal-footer">
+				<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" className="btn btn-primary">Save changes</button>
+			</div>
+		</span>			
+
+		);
+	}
+	
+}	
+
 class ModalPlus extends React.Component {
 	constructor(props) {
 		var me = super(props);
@@ -80,20 +107,9 @@ class ModalPlus extends React.Component {
 					return (			
 						<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 						  <div className="modal-dialog" role="document">
-						    <div className="modal-content">
-						      <div className="modal-header">
-							
-							<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-							  <span aria-hidden="true">&times;</span>
-							</button>
-							<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-						      </div>
-
-						      <div className="modal-footer">
-							<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-							<button type="button" className="btn btn-primary">Save changes</button>
-						      </div>
-						    </div>
+		    					<div className="modal-content">
+								<ModalContentes/>
+							</div>
 						  </div>
 						</div>		
 					);
