ed2436e48e27d69e5c479a54b38f9f05399be046
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 21:32:40 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -7,7 +7,21 @@ try {
 		}
 		pp() {
 			return (
-				<span>test pp</span>
+				<span>
+					<div className="modal-header">
+						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+					</div>
+					<div className="modal-body">
+						Test case
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" className="btn btn-primary">Save changes</button>
+					</div>
+				</span>	
 			)
 		}
 		loading(id) {
