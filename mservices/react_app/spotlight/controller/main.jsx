5cfce2c565f82ab365c38ffe2c69f7a211866803
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 21:37:17 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -1,3 +1,29 @@
+class PP extends React.Component {
+	render() {
+		return(
+			<span>
+				<div className="modal-header">
+					<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+				</div>
+				<div className="modal-body">
+					Test case pp
+				</div>
+				<div className="modal-footer">
+					<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" className="btn btn-primary">Save changes</button>
+				</div>
+			</span>			
+		
+		)
+		
+		
+	}	
+}
+
+
 try {	
 	var viewpoint = $('.'+mapping_data.id);	
 	class Home extends React.Component {
@@ -7,21 +33,7 @@ try {
 		}
 		pp() {
 			return (
-				<span>
-					<div className="modal-header">
-						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-						<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-					</div>
-					<div className="modal-body">
-						Test case
-					</div>
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
-					</div>
-				</span>	
+				<PP/>
 			)
 		}
 		loading(id) {
