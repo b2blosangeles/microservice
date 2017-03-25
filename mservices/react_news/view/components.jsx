192ae253c4a1004a3dd404c95b8e878441abfba0
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 22:44:53 -0700

    Update components.jsx

--- mservices/react_news/view/components.jsx
+++ mservices/react_news/view/components.jsx
@@ -4,7 +4,7 @@ var ListItem =  React.createClass({
 		return (
 			<div>
 				<a href="JavaScript:void(0)" onClick={this.props.parent.showDoc(this.props.item)}>{this.props.item.text}</a>
-				<button type="button" className="btn btn-default" onClick={me.props.parent.popup}>popup</button>
+				<button type="button" className="btn btn-default" onClick={me.props.parent.popup.bind(me)}>popup</button>
 			</div>
 		)
 	}	
