ade07430772c4c7fca568c0f938552a39c9ca3a3
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 18:33:44 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -111,14 +111,28 @@ class ModalPlus extends React.Component {
 							<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							  <div className="modal-dialog" role="document">
 								<div className="modal-content">
-									{me.state.ModalPlus.popupbody}
+									{me.state.ModalPlus.popupbody()}
 								</div>
 							  </div>
 							</div>		
 						)
 					} else {
 						return (
-							<ModalContentes/>
+							<span>
+								<div className="modal-header">
+									<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+								</div>
+								<div className="modal-body">
+									.===
+								</div>
+								<div className="modal-footer">
+									<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="button" className="btn btn-primary">Save changes</button>
+								</div>
+							</span>	
 						)
 					}
 				}
