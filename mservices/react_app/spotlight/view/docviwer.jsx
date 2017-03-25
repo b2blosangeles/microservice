8b073099f393860faf2144c20e0850e420b38bf6
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 22:31:12 -0700

    Update docviwer.jsx

--- mservices/react_app/spotlight/view/docviwer.jsx
+++ mservices/react_app/spotlight/view/docviwer.jsx
@@ -2,25 +2,24 @@ class Docviwer extends React.Component {
 	constructor(props) {
 		super(props);
 	}
-    render() {
-      var me = this;
-      return(
-        <span>
-        <div className="modal-header">
-          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <h5 className="modal-title" id="exampleModalLabel">{me.props.data.title}</h5>
-        </div>
-        <div className="modal-body">
-          Test case Docviwer
-        </div>
-        <div className="modal-footer">
-          <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" className="btn btn-primary">Save changes</button>
-        </div>
-        </span>			
+	render() {
+		var me = this;
+		return(
+			<span>
+				<div className="modal-header">
+					<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<h5 className="modal-title" id="exampleModalLabel">{me.props.data.title}</h5>
+				</div>
+				<div className="modal-body">
+					{me.props.data.body}
+				</div>
+				<div className="modal-footer">
+					<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+			</span>			
 
-      )
-    }	
+		)
+	}	
 }
