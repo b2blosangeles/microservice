f3bcce3db187bf7d158f4afc3425b48218aa8c7e
b2blosangeles <b2blosangeles@gmail.com>
2017-03-25 06:59:27 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -22,7 +22,9 @@ class ModalPlus extends React.Component {
 					}	
 					
 				}	
-			}				
+			} else {
+				viewpoint.find('.ModalPlus').modal('hide');
+			}
 		}, 100);
 		this.state = {ModalPlus: ''}
 	}				
