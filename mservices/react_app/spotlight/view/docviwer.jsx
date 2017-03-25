99b1d2bc897ec34645e8d63ad8cf7474f977ff54
b2blosangeles <b2blosangeles@gmail.com>
2017-03-24 21:59:49 -0700

    Update docviwer.jsx

--- mservices/react_app/spotlight/view/docviwer.jsx
+++ mservices/react_app/spotlight/view/docviwer.jsx
@@ -1,7 +1,7 @@
 class Docviwer extends React.Component {
-		constructor(props) {
-			super(props);
-		}
+	constructor(props) {
+		super(props);
+	}
     render() {
       var me = this;
       return(
@@ -10,7 +10,7 @@ class Docviwer extends React.Component {
           <button type="button" className="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
-          <h5 className="modal-title" id="exampleModalLabel">{me.props.data.title}</h5>
+          <h5 className="modal-title" id="exampleModalLabel">{me.props.data}</h5>
         </div>
         <div className="modal-body">
           Test case Docviwer
