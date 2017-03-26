2234b300c63d656d22251369741e7f1e8c5be70e
b2blosangeles <b2blosangeles@gmail.com>
2017-03-26 08:29:26 -0700

    Update main.jsx

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -74,7 +74,7 @@ try {
 							})}
 								
 							</p>	
-							<ModalPlus parent={me} level="{mapping_data.id}" />
+							<ModalPlus parent={me} level="aaa" />
 						</div>	
 					</div>
 					,
