0d600ae79b84347fc756966db3e3526e465a314f
b2blosangeles <b2blosangeles@gmail.com>
2017-03-27 10:33:42 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -52,7 +52,7 @@ class ModalPlus extends React.Component {
 				var showcloseicon = '';
 				if (!err_msg) {
 					return (			
-						<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
+						<div className={'modal fade ModalPlus ModalPlus_'+ mapping_data.id} tabindex="-1" role="dialog" aria-hidden="true">
 						  <div className="modal-dialog" role="document">
 							<div className={'alert alert-' + style} role="alert">
 								<span dangerouslySetInnerHTML={{__html: message}}></span>
@@ -72,7 +72,7 @@ class ModalPlus extends React.Component {
 				message = '<span style="color:'+textcolor+'">'+message+'</span>';
 				if (!err_msg) {
 					return (			
-						<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-hidden="true">
+						<div className={'modal fade ModalPlus ModalPlus_'+ mapping_data.id} tabindex="-1" role="dialog" aria-hidden="true">
 						  <div className="modal-dialog" role="document">
 							<div className={'text-' + style}>
 								<span dangerouslySetInnerHTML={{__html: message}}></span>
@@ -87,7 +87,7 @@ class ModalPlus extends React.Component {
 				if (!err_msg) {
 					if (me.state.ModalPlus.body) {
 						return (			
-							<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div className={'modal fade ModalPlus ModalPlus_'+ mapping_data.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							  <div className="modal-dialog" role="document">
 								<div className="modal-content">
 .									-{me.props.level}-==-{me.props.parent.state.ModalPlus.pp}=={mapping_data.id}
@@ -98,7 +98,7 @@ class ModalPlus extends React.Component {
 						)
 					} else {
 						return (
-							<div className="modal fade ModalPlus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div className={'modal fade ModalPlus ModalPlus_'+ mapping_data.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							  <div className="modal-dialog" role="document">
 								<div className="modal-content">	
 									<span>
