a2f799f45156bf7341ce86b04cee50fe3786f4ad
b2blosangeles <b2blosangeles@gmail.com>
2017-03-27 10:17:01 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -31,6 +31,7 @@ class ModalPlus extends React.Component {
 		if (prevState.ModalPlus !== me.state.ModalPlus) {
 			me.render();
 			//if (me.state.pp == mapping_data.id) {
+				console.log(viewpoint.find('.ModalPlus').length + '====----===');
 				viewpoint.find('.ModalPlus').modal({backdrop:'static'});
 				console.log(me.props.level + '==' +me.state.ModalPlus.pp + '==' +me.props.parent.state.ModalPlus.pp+'=='+mapping_data.id);
 		//	} else {
