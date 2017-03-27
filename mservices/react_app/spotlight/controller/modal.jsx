4a9a6e089c9bbe6b1583343f1cf3fb9a1eddb459
b2blosangeles <b2blosangeles@gmail.com>
2017-03-27 16:31:55 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -3,27 +3,29 @@ class ModalPlus extends React.Component {
 		var me = super(props);
 		setInterval(function(){	
 			if (me.props.parent.state.ModalPlus) {	
-				if (!me.props.parent.state.ModalPlus._id) {
-					me.props.parent.state.ModalPlus._id = true;
-					return true;
-				}	
 				if (me.props.parent.state.ModalPlus == 'cancel') {
 					viewpoint.find('.ModalPlus').modal('hide');
 					me.props.parent.state.ModalPlus = null;
-				} else {
-					if (me.props.parent.state.ModalPlus.hold) {
-						if  (!me.props.parent.state.ModalPlus.startTime) {
-							me.props.parent.state.ModalPlus.startTime = new Date().getTime();
-						}
-						if  (new Date().getTime() < (me.props.parent.state.ModalPlus.hold + me.props.parent.state.ModalPlus.startTime)) {
-							return true;
-						}
+					return true
+				}				
+				if (!me.props.parent.state.ModalPlus._id) {
+					me.props.parent.state.ModalPlus._id = true;
+					return true;
+				}		
+	
+				if (me.props.parent.state.ModalPlus.hold) {
+					if  (!me.props.parent.state.ModalPlus.startTime) {
+						me.props.parent.state.ModalPlus.startTime = new Date().getTime();
 					}
-					if (me.props.parent.state.ModalPlus !== me.state.ModalPlus) {
-						me.setState({ModalPlus: me.props.parent.state.ModalPlus });
-					}	
-
+					if  (new Date().getTime() < (me.props.parent.state.ModalPlus.hold + me.props.parent.state.ModalPlus.startTime)) {
+						return true;
+					}
+				}
+				if (me.props.parent.state.ModalPlus !== me.state.ModalPlus) {
+					me.setState({ModalPlus: me.props.parent.state.ModalPlus });
 				}	
+
+					
 			} 
 			
 		}, 50);
