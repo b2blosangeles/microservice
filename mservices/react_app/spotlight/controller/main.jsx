188f2f1cb9cdad247c76208d8e31e96092581553
b2blosangeles <b2blosangeles@gmail.com>
2017-03-28 15:19:32 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -48,14 +48,14 @@ try {
 			var me = this;
 			me.lock(e);
 
-			me.setState({ModalLoading: {textcolor:'#fff', hold:100,
-				message:'<img src="https://i.stack.imgur.com/oQ0tF.gif" width="24">'}});
+
 			
 			$.get('http://m.qalet.com/api/newsfeed/wxct/wxct_list.js',
 			{}, 
 			function (data) {
 				me.setState({ModalPlus: {type:'alert', style:'success', message:'saved'}});
-				
+			me.setState({ModalLoading: {textcolor:'#fff', hold:100,
+				message:'<img src="https://i.stack.imgur.com/oQ0tF.gif" width="24">'}});				
 				setTimeout(
 					function() {
 						me.setState({ModalLoading: 'cancel'});	
