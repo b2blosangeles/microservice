97d1a5b261e641d4e11352f5e6450e541264f1d0
b2blosangeles <b2blosangeles@gmail.com>
2017-03-28 12:10:58 -0700

    Update main.jsx

--- mservices/react_news/controller/main.jsx
+++ mservices/react_news/controller/main.jsx
@@ -82,7 +82,7 @@ try {
 					</div>
 					<div className="modal-footer">
 						<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
+						<button type="button" className="btn btn-primary" onClick={me.loading.bind(me)}>Save changes</button>
 					</div>
 				</span>	
 			)
