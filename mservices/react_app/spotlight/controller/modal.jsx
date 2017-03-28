d3c68f5af563bf328fef7b9852231325f0e11d1d
b2blosangeles <b2blosangeles@gmail.com>
2017-03-28 11:27:18 -0700

    Update modal.jsx

--- mservices/react_app/spotlight/controller/modal.jsx
+++ mservices/react_app/spotlight/controller/modal.jsx
@@ -156,3 +156,69 @@ class ModalPlus extends React.Component {
 		}
 	}				
 }
+
+class Loading extends React.Component {
+	constructor(props) {
+		var me = super(props);
+		setInterval(function(){	
+			if (me.props.parent.state.ModalPlus) {	
+				if (me.props.parent.state.ModalPlus == 'cancel') {
+					viewpoint.find('.ModalPlus').modal('hide');
+					me.props.parent.state.ModalPlus = null;
+					return true
+				}				
+				if (!me.props.parent.state.ModalPlus._id) {
+					me.props.parent.state.ModalPlus._id = true;
+					return true;
+				}		
+	
+				if (me.props.parent.state.ModalPlus.hold) {
+					if  (!me.props.parent.state.ModalPlus.startTime) {
+						me.props.parent.state.ModalPlus.startTime = new Date().getTime();
+					}
+					if  (new Date().getTime() < (me.props.parent.state.ModalPlus.hold + me.props.parent.state.ModalPlus.startTime)) {
+						return true;
+					}
+				}
+				if (me.props.parent.state.ModalPlus !== me.state.ModalPlus) {
+					me.setState({ModalPlus: me.props.parent.state.ModalPlus });
+				}	
+
+					
+			} 
+			
+		}, 50);
+		this.state = {ModalPlus: ''}
+	}				
+	componentDidUpdate (prevProps, prevState) {
+		var me = this;
+		if (prevState.ModalPlus !== me.state.ModalPlus) {
+			// me.render();
+			viewpoint.find('.ModalPlus_'+ mapping_data.id).modal({backdrop:'static'});
+		} 
+	}
+	modalClass () {
+		return 'modal fade ModalPlus ModalPlus_'+ mapping_data.id;
+	}	
+	render () {
+		var me = this, err_msg = '';
+		if (_modal_backdrop_) {
+			if (me.state.ModalPlus.backdrop)  _modal_backdrop_.set(me.state.ModalPlus.backdrop);	
+			else  _modal_backdrop_.resetDefault();
+		}
+		var message = '', box_style={};
+		var message = (me.state.ModalPlus.message)?(me.state.ModalPlus.message):'Loading ...';
+		box_style = (me.state.ModalPlus.box_style)?me.state.ModalPlus.box_style:{color:'#fff'};
+		if (!err_msg) {
+			return (			
+				<div className={me.modalClass()} tabindex="-1" role="dialog" aria-hidden="true">
+				  <div className="modal-dialog" role="document">
+					<div style={box_style}>
+						<span dangerouslySetInnerHTML={{__html: message}}></span>
+					</div>
+				  </div>
+				</div>	
+			);
+		}
+	}				
+}
