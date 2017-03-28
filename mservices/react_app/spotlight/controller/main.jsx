6a6a553adf5dd121faf1a6d5b7497768e16d5669
b2blosangeles <b2blosangeles@gmail.com>
2017-03-28 15:21:48 -0700

    Update main.jsx

--- mservices/react_app/spotlight/controller/main.jsx
+++ mservices/react_app/spotlight/controller/main.jsx
@@ -53,7 +53,7 @@ try {
 			$.get('http://m.qalet.com/api/newsfeed/wxct/wxct_list.js',
 			{}, 
 			function (data) {
-				me.setState({ModalPlus: {type:'alert', style:'success A', message:'saved'}});
+				me.setState({ModalPlus: {type:'alert', style:'success', message:'saved A'}});
 			me.setState({ModalLoading: {textcolor:'#fff', hold:100,
 				message:'<img src="https://i.stack.imgur.com/oQ0tF.gif" width="24">'}});				
 				setTimeout(
