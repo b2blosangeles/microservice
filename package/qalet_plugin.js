70be6e72c407b790efefb936cf3f4751715a32d5
b2blosangeles <b2blosangeles@gmail.com>
2017-03-31 15:09:59 -0700

    Update qalet_plugin.js

--- package/qalet_plugin.js
+++ package/qalet_plugin.js
@@ -16,9 +16,9 @@
 		"spotlight":[
 			"/mservices/react_app/spotlight/controller/modal-backdrop.js",
 			"/mservices/react_app/spotlight/controller/modal.jsx",
-			"/mservices/react_app/spotlight/controller/modalA.jsx",
 			"/mservices/react_app//spotlight/view/docviwer.jsx",
-			"/mservices/react_app/spotlight/controller/main.jsx"
+			"/mservices/react_app/spotlight/controller/main.jsx",
+			"/mservices/react_app/spotlight/controller/modalA.jsx"
 		]
 			
 	}, 
