061bde79cc629c8bc77756d18c1897f2605b3b47
b2blosangeles <b2blosangeles@gmail.com>
2017-03-31 06:18:18 -0700

    Update modalA.jsx

--- mservices/react_app/spotlight/controller/modalA.jsx
+++ mservices/react_app/spotlight/controller/modalA.jsx
@@ -1,5 +1,5 @@
 
-if (!ModalPlusAA) {
+if (!ModalPlusA) {
 	class ModalPlusAA extends React.Component {
 		constructor(props) {
 			var me = super(props);
@@ -21,101 +21,13 @@ if (!ModalPlusAA) {
 		}	
 		render () {
 			var me = this, err_msg = '';
-			if (_modal_backdrop_) {
-				if (me.state.ModalPlus.backdrop)  _modal_backdrop_.set(me.state.ModalPlus.backdrop);	
-				else  _modal_backdrop_.resetDefault();
-			}
-			switch(me.state.ModalPlus.type) {
-				case "alert":
-					var box_class = '', box_style = '', message = '', close_icon = true;
+		//	if (_modal_backdrop_) {
+		//		if (me.state.ModalPlus.backdrop)  _modal_backdrop_.set(me.state.ModalPlus.backdrop);	
+		//		else  _modal_backdrop_.resetDefault();
+		//	}
 
-					box_class = (me.state.ModalPlus.box_class)?me.state.ModalPlus.box_class:'info';
-					message = (me.state.ModalPlus.message)?('<strong>!</strong> ' + me.state.ModalPlus.message):'<strong>!</strong>';
-					close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
-					box_style = (me.state.ModalPlus.box_style)?me.state.ModalPlus.box_style:{};
 
-					if (!err_msg) {
-						return (			
-							<div className={me.modalClass()} tabindex="-1" role="dialog" aria-hidden="true">
-							  <div className="modal-dialog" role="document">
-								<div className={'alert alert-' + box_class} style={box_style} role="alert">
-									<span dangerouslySetInnerHTML={{__html: message}}></span>
-									<button type="button" className="close" data-dismiss="modal" style={{display:close_icon}}>
-										&times;
-									</button>
-								</div>
-							  </div>
-							</div>	
-						);
-					}
-					break;	
-				case "popup":
-
-					var box_class = '', box_style = '', message = '', close_icon = true;
-
-					box_class = (me.state.ModalPlus.box_class)?me.state.ModalPlus.box_class:'info';
-					message = (me.state.ModalPlus.message)?('<strong>!</strong> ' + me.state.ModalPlus.message):'<strong>!</strong>';
-					close_icon = (me.state.ModalPlus.close_icon === false)?'none':'';
-					box_style = (me.state.ModalPlus.box_style)?me.state.ModalPlus.box_style:{};
-
-					if (!err_msg) {
-						if (me.state.ModalPlus.body) {
-							return (			
-								<div className={me.modalClass()} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-								  <div className="modal-dialog" role="document">
-									<div className={'modal-content ' + box_class} style={box_style} >
-										{me.state.ModalPlus.body}
-									</div>
-								  </div>
-								</div>		
-							)
-						} else {
-							return (
-								<div className={me.modalClass()} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-								  <div className="modal-dialog" role="document">
-									<div className={'modal-content ' + box_class} style={box_style} >	
-										<span>
-											<div className="modal-header">
-												<button type="button" className="close" data-dismiss="modal" style={{display:close_icon}}>
-													&times;
-												</button>
-												<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-											</div>
-											<div className="modal-body">
-												{/*<span dangerouslySetInnerHTML={{__html: message}}></span>*/}
-												{me.state.ModalPlus.message}
-											</div>
-											<div className="modal-footer">
-												<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-												<button type="button" className="btn btn-primary">Save changes</button>
-											</div>
-										</span>	
-									</div>
-								  </div>
-								</div>										
-							)
-						}
-					}
-					break;				
-				default:
-					err_msg = 'wrong or missong ModelPlus Type';
-			} 
-			if (err_msg) {
-				return (
-					<div className={me.modalClass()} tabindex="-1" role="dialog" aria-hidden="true">
-					  <div className="modal-dialog" role="document">
-						<div className="alert alert-danger" role="alert">
-							<strong>!</strong> {err_msg}
-							<button type="button" className="close" data-dismiss="modal">
-								&times;
-							</button>
-						</div>
-					  </div>
-					</div>	
-				);
-			} else {
-				return(<span></span>);
-			}
+			return(<span></span>);
 		}				
 	}
 	/*
